13e120d8f1de7b9330c54560516502873a157474
Salim <eldjsalim@gmail.com>
2025-10-09 11:41:06 +0200

    default db psswd

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ import mysql from "mysql2/promise";
 export const pool = mysql.createPool({
   host: process.env.DB_HOST || "db",
   user: process.env.DB_USER || "user",
-  password: process.env.DB_PASSWORD || "userpassword",
+  password: process.env.DB_PASSWORD || "root",
   database: process.env.DB_NAME || "time_manager",
   waitForConnections: true,
   connectionLimit: 10, // number of simultaneous connections
