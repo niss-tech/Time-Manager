544c4defc2a5f499164cc279dd43fe2194303d0c
Salim <eldjsalim@gmail.com>
2025-10-09 11:38:17 +0200

    base index.js & api setup

---
+++ backend/db.js
@@ -0,0 +1,13 @@
+import mysql from "mysql2/promise";
+
+export const pool = mysql.createPool({
+  host: process.env.DB_HOST || "db",
+  user: process.env.DB_USER || "user",
+  password: process.env.DB_PASSWORD || "userpassword",
+  database: process.env.DB_NAME || "time_manager",
+  waitForConnections: true,
+  connectionLimit: 10, // number of simultaneous connections
+  queueLimit: 0,
+});
+
+console.log("✅ MariaDB pool created");
