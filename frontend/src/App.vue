d003ccc70639cba6dbe4d8043db9d105df1e6fce
Salim <eldjsalim@gmail.com>
2025-10-10 11:52:55 +0200

    login page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,19 +1,14 @@
 <template>
-  <div id="app">
     <Header />
-
     <router-view></router-view>
-  </div>
 </template>
 
 <script setup>
-import Header from './components/Header.vue'
+// import Header from './components/Header.vue'
 import Home from './pages/Home.vue'
 import Login from './pages/Login.vue'
 </script>
 
 <style>
-#app {
-  font-family: Arial, sans-serif;
-}
+
 </style>
