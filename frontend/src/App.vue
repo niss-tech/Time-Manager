79bcb3c091e3d09eee05a9ff89d8a5b54d2e9143
Salim <eldjsalim@gmail.com>
2025-10-10 16:37:52 +0200

    profile § dashboard init

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,28 @@
 <template>
-    <Header />
-    <router-view></router-view>
+    <div>
+        <template v-if="route.path === '/Login'">
+            <router-view />
+        </template>
+        <template v-else>
+            <div class="d-flex">
+                <SideNav />
+                <div class="flex-fill d-flex flex-column">
+                    <Header />
+                    <div class="flex-fill">
+                        <router-view />
+                    </div>
+                </div>
+            </div>
+        </template>
+    </div>
 </template>
 
 <script setup>
-// import Header from './components/Header.vue'
-import Home from './pages/Home.vue'
-import Login from './pages/Login.vue'
+import { useRoute } from 'vue-router'
+import Header from './components/Header.vue'
+import SideNav from './components/SideNav.vue'
+
+const route = useRoute()
 </script>
 
 <style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,9 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../pages/Home.vue'
 import Login from '../pages/Login.vue'
+import Profile from '../pages/Profile.vue'
 
 const routes = [
   { path: '/', component: Home },
+  { path: '/Profile', component: Profile },
   { path: '/Login', component: Login },
 ]
 
