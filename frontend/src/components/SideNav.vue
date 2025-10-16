d146d3b810fe47e4e119e5b631f61dc51bb7a4ee
Salim <eldjsalim@gmail.com>
2025-10-16 15:51:01 +0200

    dashboard and sidenav upt

--- frontend/src/components/SideNav.vue
+++ frontend/src/components/SideNav.vue
@@ -11,7 +11,12 @@
     <nav>
       <ul class="nav flex-column">
         <li class="nav-item mb-2">
-          <router-link to="/" class="nav-link" :class="{ active: isActive('/') }">Dashboard</router-link>
+          <router-link to="/" class="nav-link" :class="{ active: isActive('/') }">
+            <div class="d-flex align-items-center">              
+              <svg class="w-25 icon flat-color" fill="#000000" viewBox="0 0 24 24" id="dashboard" data-name="Flat Color" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path id="secondary" d="M22,4V7a2,2,0,0,1-2,2H15a2,2,0,0,1-2-2V4a2,2,0,0,1,2-2h5A2,2,0,0,1,22,4ZM9,15H4a2,2,0,0,0-2,2v3a2,2,0,0,0,2,2H9a2,2,0,0,0,2-2V17A2,2,0,0,0,9,15Z" style="fill: #2ca9bc;"></path><path id="primary" d="M11,4v7a2,2,0,0,1-2,2H4a2,2,0,0,1-2-2V4A2,2,0,0,1,4,2H9A2,2,0,0,1,11,4Zm9,7H15a2,2,0,0,0-2,2v7a2,2,0,0,0,2,2h5a2,2,0,0,0,2-2V13A2,2,0,0,0,20,11Z" style="fill: #000000;"></path></g></svg>
+              <span>Dashboard</span>
+            </div>
+          </router-link>
         </li>
         <li class="nav-item mb-2">
           <router-link to="/Profile" class="nav-link" :class="{ active: isActive('/Profile') }">Profile</router-link>
