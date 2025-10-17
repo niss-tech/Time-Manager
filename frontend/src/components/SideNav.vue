a4dee3ab4db618aaf4499f912ccb16e82b7ae627
Salim <eldjsalim@gmail.com>
2025-10-17 11:04:47 +0200

    dashboard & sidenav update

--- frontend/src/components/SideNav.vue
+++ frontend/src/components/SideNav.vue
@@ -1,5 +1,5 @@
 <template>
-  <aside class="sidenav bg-light border-end p-3 d-none d-md-block">
+  <aside class="sidenav bg-subtle border-end p-3 d-none d-md-block">
     <div class="profile d-flex align-items-center mb-3">
       <img :src="avatar" alt="avatar" class="avatar me-2"/>
       <div>
@@ -9,23 +9,38 @@
     </div>
 
     <nav>
-      <ul class="nav flex-column">
-        <li class="nav-item mb-2">
+      <ul class="nav text-black fw-semibold flex-column">
+        <li class="mb-2">
           <router-link to="/" class="nav-link" :class="{ active: isActive('/') }">
-            <div class="d-flex align-items-center">              
-              <svg class="w-25 icon flat-color" fill="#000000" viewBox="0 0 24 24" id="dashboard" data-name="Flat Color" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path id="secondary" d="M22,4V7a2,2,0,0,1-2,2H15a2,2,0,0,1-2-2V4a2,2,0,0,1,2-2h5A2,2,0,0,1,22,4ZM9,15H4a2,2,0,0,0-2,2v3a2,2,0,0,0,2,2H9a2,2,0,0,0,2-2V17A2,2,0,0,0,9,15Z" style="fill: #2ca9bc;"></path><path id="primary" d="M11,4v7a2,2,0,0,1-2,2H4a2,2,0,0,1-2-2V4A2,2,0,0,1,4,2H9A2,2,0,0,1,11,4Zm9,7H15a2,2,0,0,0-2,2v7a2,2,0,0,0,2,2h5a2,2,0,0,0,2-2V13A2,2,0,0,0,20,11Z" style="fill: #000000;"></path></g></svg>
+            <div class="d-flex align-items-center gap-3">
+              <svg class="w-10 icon flat-color" fill="#000000" viewBox="0 0 24 24" id="dashboard" data-name="Flat Color" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path id="secondary" d="M22,4V7a2,2,0,0,1-2,2H15a2,2,0,0,1-2-2V4a2,2,0,0,1,2-2h5A2,2,0,0,1,22,4ZM9,15H4a2,2,0,0,0-2,2v3a2,2,0,0,0,2,2H9a2,2,0,0,0,2-2V17A2,2,0,0,0,9,15Z" style="fill: #2ca9bc;"></path><path id="primary" d="M11,4v7a2,2,0,0,1-2,2H4a2,2,0,0,1-2-2V4A2,2,0,0,1,4,2H9A2,2,0,0,1,11,4Zm9,7H15a2,2,0,0,0-2,2v7a2,2,0,0,0,2,2h5a2,2,0,0,0,2-2V13A2,2,0,0,0,20,11Z" style="fill: #000000;"></path></g></svg>
               <span>Dashboard</span>
             </div>
           </router-link>
         </li>
-        <li class="nav-item mb-2">
-          <router-link to="/Profile" class="nav-link" :class="{ active: isActive('/Profile') }">Profile</router-link>
+        <li class="mb-2">
+          <router-link to="/Profile" class="nav-link" :class="{ active: isActive('/Profile') }">
+            <div class="d-flex align-items-center gap-3">
+              <svg class="w-10" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M12.1207 12.78C12.0507 12.77 11.9607 12.77 11.8807 12.78C10.1207 12.72 8.7207 11.28 8.7207 9.50998C8.7207 7.69998 10.1807 6.22998 12.0007 6.22998C13.8107 6.22998 15.2807 7.69998 15.2807 9.50998C15.2707 11.28 13.8807 12.72 12.1207 12.78Z" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M18.7398 19.3801C16.9598 21.0101 14.5998 22.0001 11.9998 22.0001C9.39977 22.0001 7.03977 21.0101 5.25977 19.3801C5.35977 18.4401 5.95977 17.5201 7.02977 16.8001C9.76977 14.9801 14.2498 14.9801 16.9698 16.8001C18.0398 17.5201 18.6398 18.4401 18.7398 19.3801Z" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" stroke="#1b93b1" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+              <span>Profile</span>
+            </div>
+          </router-link>
         </li>
         <li class="nav-item mb-2">
-          <router-link to="/Teams" class="nav-link" :class="{ active: isActive('/Teams') }">Teams</router-link>
+          <router-link to="/Teams" class="nav-link" :class="{ active: isActive('/Teams') }">
+            <div class="d-flex align-items-center gap-3">
+              <svg class="icon w-10" fill="#1b93b1" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" ><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M824.2 699.9a301.55 301.55 0 0 0-86.4-60.4C783.1 602.8 812 546.8 812 484c0-110.8-92.4-201.7-203.2-200-109.1 1.7-197 90.6-197 200 0 62.8 29 118.8 74.2 155.5a300.95 300.95 0 0 0-86.4 60.4C345 754.6 314 826.8 312 903.8a8 8 0 0 0 8 8.2h56c4.3 0 7.9-3.4 8-7.7 1.9-58 25.4-112.3 66.7-153.5A226.62 226.62 0 0 1 612 684c60.9 0 118.2 23.7 161.3 66.8C814.5 792 838 846.3 840 904.3c.1 4.3 3.7 7.7 8 7.7h56a8 8 0 0 0 8-8.2c-2-77-33-149.2-87.8-203.9zM612 612c-34.2 0-66.4-13.3-90.5-37.5a126.86 126.86 0 0 1-37.5-91.8c.3-32.8 13.4-64.5 36.3-88 24-24.6 56.1-38.3 90.4-38.7 33.9-.3 66.8 12.9 91 36.6 24.8 24.3 38.4 56.8 38.4 91.4 0 34.2-13.3 66.3-37.5 90.5A127.3 127.3 0 0 1 612 612zM361.5 510.4c-.9-8.7-1.4-17.5-1.4-26.4 0-15.9 1.5-31.4 4.3-46.5.7-3.6-1.2-7.3-4.5-8.8-13.6-6.1-26.1-14.5-36.9-25.1a127.54 127.54 0 0 1-38.7-95.4c.9-32.1 13.8-62.6 36.3-85.6 24.7-25.3 57.9-39.1 93.2-38.7 31.9.3 62.7 12.6 86 34.4 7.9 7.4 14.7 15.6 20.4 24.4 2 3.1 5.9 4.4 9.3 3.2 17.6-6.1 36.2-10.4 55.3-12.4 5.6-.6 8.8-6.6 6.3-11.6-32.5-64.3-98.9-108.7-175.7-109.9-110.9-1.7-203.3 89.2-203.3 199.9 0 62.8 28.9 118.8 74.2 155.5-31.8 14.7-61.1 35-86.5 60.4-54.8 54.7-85.8 126.9-87.8 204a8 8 0 0 0 8 8.2h56.1c4.3 0 7.9-3.4 8-7.7 1.9-58 25.4-112.3 66.7-153.5 29.4-29.4 65.4-49.8 104.7-59.7 3.9-1 6.5-4.7 6-8.7z"></path> </g></svg>
+              <span>Teams</span>
+            </div>
+          </router-link>
         </li>
         <li class="nav-item mb-2">
-          <router-link to="/Login" class="nav-link" :class="{ active: isActive('/Login') }">Login</router-link>
+          <router-link to="/Login" class="nav-link" :class="{ active: isActive('/Login') }">
+            <div class="d-flex align-items-center gap-3">
+              <svg class="w-10" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M21 12L13 12" stroke="#1b93b1" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M18 15L20.913 12.087V12.087C20.961 12.039 20.961 11.961 20.913 11.913V11.913L18 9" stroke="#1b93b1" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M16 5V4.5V4.5C16 3.67157 15.3284 3 14.5 3H5C3.89543 3 3 3.89543 3 5V19C3 20.1046 3.89543 21 5 21H14.5C15.3284 21 16 20.3284 16 19.5V19.5V19" stroke="#1b93b1" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+              <span>Logout</span>
+            </div>
+          </router-link>
         </li>
       </ul>
     </nav>
@@ -42,7 +57,7 @@
         <li class="nav-item mb-2"><router-link to="/" class="nav-link">Dashboard</router-link></li>
         <li class="nav-item mb-2"><router-link to="/Profile" class="nav-link">Profile</router-link></li>
         <li class="nav-item mb-2"><router-link to="/Teams" class="nav-link">Teams</router-link></li>
-        <li class="nav-item mb-2"><router-link to="/Login" class="nav-link">Login</router-link></li>
+        <li class="nav-item mb-2"><router-link to="/Login" class="nav-link">Logout</router-link></li>
       </ul>
     </div>
   </div>
@@ -95,13 +110,14 @@ function isActive(path) {
   display: block;
   padding-bottom: 0.35rem;
   border-bottom: 3px solid transparent; /* reserve space */
+  color: black;
 }
 
 .nav .nav-link.router-link-active,
 .nav .nav-link.router-link-exact-active,
 .nav .nav-link.active {
   font-weight: 700;
-  color: #0d6efd;
-  border-bottom-color: #0d6efd;
+  color: #1b93b1;
+  border-bottom-color: #1b93b1;
 }
 </style>
--- frontend/src/pages/Dashboard.vue
+++ frontend/src/pages/Dashboard.vue
@@ -8,7 +8,7 @@
             <h5>Worked This Week</h5>
             <div class="d-flex justify-content-center gap-3 align-items-center">
               <div class="75">
-                <h3 class="fw-bold text-center">01:07:00</h3>
+                <h3 class="fw-bold text-center">37:12:23</h3>
               </div>
               <div class="d-flex justify-content-center bg-primary-subtle rounded-4 p-3">
                 <svg style="height: 50px;" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M22.7 11.5L20.7005 13.5L18.7 11.5M20.9451 13C20.9814 12.6717 21 12.338 21 12C21 7.02944 16.9706 3 12 3C7.02944 3 3 7.02944 3 12C3 16.9706 7.02944 21 12 21C14.8273 21 17.35 19.6963 19 17.6573M12 7V12L15 14" stroke="#1b93b1" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
@@ -21,7 +21,7 @@
             <h5>Today's Work Time</h5>
             <div class="d-flex flex-row-reverse justify-content-evenly w-100 gap-3 align-items-center">
               <div class="">
-                <h2 class="display-5 fw-bold text-center">37:12:23</h2>
+                <h2 class="display-5 fw-bold text-center">01:18:55</h2>
               </div>
               <div class="d-flex justify-content-between bg-primary-subtle rounded-4 p-3">
                 <svg style="height: 50px;" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M20 10V7C20 5.89543 19.1046 5 18 5H6C4.89543 5 4 5.89543 4 7V10M20 10V19C20 20.1046 19.1046 21 18 21H6C4.89543 21 4 20.1046 4 19V10M20 10H4M8 3V7M16 3C16 3 16 5.4379 16 7" stroke="#1b93b1" stroke-width="2" stroke-linecap="round"></path> <rect x="6" y="12" width="5" height="5" rx="1" fill="#1b93b1"></rect> </g></svg>
