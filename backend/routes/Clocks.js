5e5e4f0989f66c66c636231ae3090605e0985570
Salim <eldjsalim@gmail.com>
2025-10-17 14:59:28 +0200

    fix clocks import

--- backend/routes/Clocks.js
+++ backend/routes/Clocks.js
@@ -1,5 +1,5 @@
 import express from "express";
-import ClocksController from "../../controllers/ClocksController.js";
+import ClocksController from "../controllers/ClocksController.js";
 
 const router = express.Router();
 
